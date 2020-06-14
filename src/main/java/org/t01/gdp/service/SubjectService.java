83d0037b579ac74cffd38ad0ccf5473460188f87
PengxinZeng <2279296959@qq.com>
2020-06-14 23:16:03 +0800

    Replace SubjectService.java

--- src/main/java/org/t01/gdp/service/SubjectService.java
+++ src/main/java/org/t01/gdp/service/SubjectService.java
@@ -7,7 +7,6 @@ import org.springframework.stereotype.Service;
 import org.t01.gdp.domain.*;
 import org.t01.gdp.mapper.*;
 
-import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,17 +23,20 @@ public class SubjectService {
 
     @Autowired
     MajorMapper majorMapper;
-
     @Autowired
-    SqlMapper sqlMapper;
+    SubjectMajorTeacherMapper subjectMajorTeacherMapper;
 
     public void updateById(Subject subject, String examine_flag) {
-            subject.setState(examine_flag);
-            subjectMapper.updateByPrimaryKeySelective(subject);
+        subject.setState(examine_flag);
+        subjectMapper.updateByPrimaryKeySelective(subject);
     }
 
-    public PageInfo<StudentAndSubject> getSubjectsByReviewTeacherId(int pageNo, int pageSize, String reviewTeacherId){
-        PageHelper.startPage(pageNo,pageSize);
+    public int updateWithSubject(Subject subject) {
+        return subjectMapper.updateByPrimaryKey(subject);
+    }
+
+    public PageInfo<StudentAndSubject> getSubjectsByReviewTeacherId(int pageNo, int pageSize, String reviewTeacherId) {
+        PageHelper.startPage(pageNo, pageSize);
         //查询reviewTeacherId中有一个与传入的id相同的subject
         SubjectExample subjectExample1 = new SubjectExample();
         subjectExample1.createCriteria().andReviewTeacherId1EqualTo(reviewTeacherId);
@@ -56,52 +58,59 @@ public class SubjectService {
             StudentExample studentExample = new StudentExample();
             studentExample.createCriteria().andSubjectIdEqualTo(allSubject.getId());
             List<Student> students = studentMapper.selectByExample(studentExample);
-            if(students.size()>0){
+            if (students.size() > 0) {
                 for (Student student : students) {
                     studentAndSubjectList.add(new StudentAndSubject(student.getId(), allSubject.getId().toString(),
-                            allSubject.getName(),allSubject.getMajorId(),allSubject.getDirection()));
+                            allSubject.getName(), allSubject.getMajorId(), allSubject.getDirection()));
                 }
             }
         }
         return new PageInfo<>(studentAndSubjectList);
     }
 
-    public boolean chooseSubjectBystudent(String student_id, String subject_id){
-        Student student=new Student();
+    public PageInfo<Subject> getSubjects(int pageNo, int pageSize) {
+        PageHelper.startPage(pageNo, pageSize);
+        return new PageInfo<>(subjectMapper.selectAll());
+    }
+    public PageInfo<SubjectMajorTeacher> getSubjectsByTeacherId(int pageNo, int pageSize, String id) {
+        PageHelper.startPage(pageNo, pageSize);
+        return new PageInfo<>(subjectMajorTeacherMapper.selectByTeacherId(id));
+    }
+
+    public void chooseSubjectBystudent(String student_id, String subject_id) {
+        Student student = new Student();
         student.setSubjectId(Long.valueOf(subject_id));
         student.setId(student_id);
         studentMapper.updateByPrimaryKeySelective(student);
-        return true;
+        return;
     }
 
-    public SubjectInfo selectSubjectBystudent(String subject_id){
-        SubjectInfo subjectInfo;
-        subjectInfo=sqlMapper.selectSubjectByPrimaryKey(Long.valueOf(subject_id));
-        return subjectInfo;
+    public Subject selectSubjectBystudent(String subject_id) {
+        Subject subject;
+        subject = subjectMapper.selectByPrimaryKey(Long.valueOf(subject_id));
+        return subject;
     }
 
-    public PageInfo<SubjectInfo> listSubjectBystudent(int pageNo,
-                                                      int pageSize,
-                                                      String subject_name,
-                                                      String subject_teacher,
-                                                      String subject_major,
-                                                      String subject_ID,
-                                                      String subject_direction,
-                                                      String difficult_mn,
-                                                      String difficult_mx) {
-        PageHelper.startPage(pageNo,pageSize);
-        BigDecimal difficult_min=(difficult_mn!=""?new BigDecimal(difficult_mn):null);
-        BigDecimal difficult_max=(difficult_mx!=""?new BigDecimal(difficult_mx):null);
-        Long subject_id=(subject_ID!=""?new Long(subject_ID):null);
-        List<SubjectInfo> list=sqlMapper.selectBycondition(
-                subject_name,
-                subject_teacher,
-                subject_major,
-                subject_id,
-                subject_direction,
-                difficult_min,
-                difficult_max);
-        PageInfo<SubjectInfo> subjectPageInfo=new PageInfo<>(list);
-        return subjectPageInfo;
+    public List listSubjectBystudent(Listsubject listsubject) {
+        UserExample userExample = new UserExample();                                                     //根据教师名字查询教师id
+        userExample.createCriteria().andNameEqualTo(listsubject.getCreate_teacher());
+        List<User> userlist = userMapper.selectByExample(userExample);
+        String create_teacher_id = (userlist.size() > 0 ? userlist.get(0).getId() : null);
+
+        MajorExample majorExample = new MajorExample();                                                  //根据专业名字查询专业id
+        majorExample.createCriteria().andNameEqualTo(listsubject.getMayjor());
+        List<Major> majorlist = majorMapper.selectByExample(majorExample);
+        String mayjor_id = (majorlist.size() > 0 ? majorlist.get(0).getId() : null);
+
+        SubjectExample subjectExample = new SubjectExample();                                             //条件查询subject
+        subjectExample.createCriteria().andIdEqualTo(listsubject.getSubject_id())
+                .andCreateTeacherIdEqualTo(create_teacher_id)
+                .andMajorIdEqualTo(mayjor_id)
+                .andNameLike(listsubject.getName())
+                .andDirectionEqualTo(listsubject.getDirection())
+                .andDifficultyBetween(listsubject.getDifficult_min(), listsubject.getDifficult_max());
+        List<Subject> list = subjectMapper.selectByExample(subjectExample);
+
+        return list;
     }
 }
