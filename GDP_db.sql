04a875ea5c41703092ff73b5104266488d3a1850
YeKui <1600677562@qq.com>
2020-06-15 14:02:04 +0800

    数据库修改

--- GDP_db.sql
+++ GDP_db.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `gdp` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `gdp`;
 -- MySQL dump 10.13  Distrib 8.0.20, for Win64 (x86_64)
 --
 -- Host: localhost    Database: gdp
@@ -32,6 +34,15 @@ CREATE TABLE `administrator` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `administrator`
+--
+
+LOCK TABLES `administrator` WRITE;
+/*!40000 ALTER TABLE `administrator` DISABLE KEYS */;
+/*!40000 ALTER TABLE `administrator` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `college`
 --
 
@@ -47,6 +58,16 @@ CREATE TABLE `college` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `college`
+--
+
+LOCK TABLES `college` WRITE;
+/*!40000 ALTER TABLE `college` DISABLE KEYS */;
+INSERT INTO `college` VALUES ('1','1','1','1','1');
+/*!40000 ALTER TABLE `college` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `major`
@@ -67,6 +88,16 @@ CREATE TABLE `major` (
   CONSTRAINT `major_college_id` FOREIGN KEY (`college_id`) REFERENCES `college` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `major`
+--
+
+LOCK TABLES `major` WRITE;
+/*!40000 ALTER TABLE `major` DISABLE KEYS */;
+INSERT INTO `major` VALUES ('1','1','1','1','1','1');
+/*!40000 ALTER TABLE `major` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `student`
@@ -99,6 +130,17 @@ CREATE TABLE `student` (
   `teacher_paper_score` int DEFAULT NULL,
   `student_paper_score` int DEFAULT NULL,
   `final_score` decimal(5,2) DEFAULT NULL,
+  `open_evaluation1` varchar(1023) DEFAULT NULL,
+  `open_evaluation2` varchar(1023) DEFAULT NULL,
+  `open_evaluation3` varchar(1023) DEFAULT NULL,
+  `middle_evaluation1` varchar(1023) DEFAULT NULL,
+  `middle_evaluation2` varchar(1023) DEFAULT NULL,
+  `middle_evaluation3` varchar(1023) DEFAULT NULL,
+  `teacher_paper_evaluation` varchar(1023) DEFAULT NULL,
+  `cross_paper_evaluation` varchar(45) DEFAULT NULL,
+  `conclusion_evaluation1` varchar(45) DEFAULT NULL,
+  `conclusion_evaluation2` varchar(45) DEFAULT NULL,
+  `conclusion_evaluation3` varchar(45) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `student_cross_student_id_idx` (`cross_student_id`),
   KEY `student_major_id` (`major_id`),
@@ -107,6 +149,16 @@ CREATE TABLE `student` (
   CONSTRAINT `student_user_id` FOREIGN KEY (`id`) REFERENCES `user` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `student`
+--
+
+LOCK TABLES `student` WRITE;
+/*!40000 ALTER TABLE `student` DISABLE KEYS */;
+INSERT INTO `student` VALUES ('323','1','1','1','1',1,NULL,NULL,NULL,NULL,'1',NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,99,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL,NULL);
+/*!40000 ALTER TABLE `student` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `subject`
@@ -141,8 +193,18 @@ CREATE TABLE `subject` (
   CONSTRAINT `subject_teacher_id1` FOREIGN KEY (`review_teacher_id1`) REFERENCES `teacher` (`id`),
   CONSTRAINT `subject_teacher_id2` FOREIGN KEY (`review_teacher_id2`) REFERENCES `teacher` (`id`),
   CONSTRAINT `subject_teacher_id3` FOREIGN KEY (`review_teacher_id3`) REFERENCES `teacher` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `subject`
+--
+
+LOCK TABLES `subject` WRITE;
+/*!40000 ALTER TABLE `subject` DISABLE KEYS */;
+INSERT INTO `subject` VALUES (1,'223','1','1','1',0.10,'1','1','','223',NULL,NULL,'NEW','2020-06-14 06:17:46'),(2,'223','1','1','1',0.10,'1','1','','223',NULL,NULL,'NEW','2020-06-14 06:17:46'),(7,'223','1','1','1',0.10,'1','1','','223',NULL,NULL,'NEW','2020-06-14 06:17:46'),(8,'223','1','1','1',0.10,'1','1','','223',NULL,NULL,'NEW','2020-06-14 06:17:46'),(9,'223','1','1','1',0.10,'1','1','','223',NULL,NULL,'NEW','2020-06-14 06:17:46'),(10,'223','1','1','1',0.10,'1','1','','223',NULL,NULL,'NEW','2020-06-14 06:17:46'),(11,'223','1','1','1',0.10,'1','1','','223',NULL,NULL,'NEW','2020-06-14 06:17:46'),(12,'223','1','1','1',0.10,'1','1','','223',NULL,NULL,'NEW','2020-06-14 06:17:46'),(13,'223','1','1','1',0.10,'1','1','','223',NULL,NULL,'NEW','2020-06-14 06:17:46'),(14,'223','1','1','1',0.10,'1','1','','223',NULL,NULL,'NEW','2020-06-14 06:17:46');
+/*!40000 ALTER TABLE `subject` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `teacher`
@@ -165,6 +227,16 @@ CREATE TABLE `teacher` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
+-- Dumping data for table `teacher`
+--
+
+LOCK TABLES `teacher` WRITE;
+/*!40000 ALTER TABLE `teacher` DISABLE KEYS */;
+INSERT INTO `teacher` VALUES ('223','1',NULL,NULL,NULL);
+/*!40000 ALTER TABLE `teacher` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `user`
 --
 
@@ -182,6 +254,16 @@ CREATE TABLE `user` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user`
+--
+
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES ('123','123456','ADM','张三',NULL,NULL,'2020-06-09 09:34:10'),('223','123456','TEA','李四',NULL,NULL,'2020-06-09 11:59:13'),('323','123456','STU','王五',NULL,NULL,'2020-06-09 11:59:13');
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -192,4 +274,4 @@ CREATE TABLE `user` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-06-09 17:22:31
+-- Dump completed on 2020-06-15 13:58:01
