b38724d874c325863390ef42937ab075d913b146
panzihao <biosfire3367>
2020-06-21 19:23:58 +0800

    完成了教师首页

--- src/main/java/org/t01/gdp/service/TeacherService.java
+++ src/main/java/org/t01/gdp/service/TeacherService.java
@@ -18,6 +18,10 @@ public class TeacherService {
     MajorMapper majorMapper;
     @Autowired
     SubjectService subjectService;
+    @Autowired
+    CollegeMapper collegeMapper;
+    @Autowired
+    StudentMapper studentMapper;
 
     public Teacher selectTeacherById(long teacherId){
         return teacherMapper.selectByPrimaryKey(teacherId);
@@ -120,4 +124,148 @@ public class TeacherService {
     public PageInfo<StudentAndSubject> cross_review_select(ReviewSearchCase reviewSearchCase,int pageNo,int pageSize,Long teacher_id){
         return subjectService.searchReview(reviewSearchCase,pageNo,pageSize);
     }//查看交叉评阅列表
+
+    public Map<String,Object> simpleSelect(Long teacher_id){
+        Map<String,Object> map=new HashMap<String,Object>();
+        Teacher teacher=teacherMapper.selectByPrimaryKey(teacher_id);
+        College college=collegeMapper.selectByPrimaryKey(teacher.getCollegeId());
+        map.put("name",teacher.getName());
+        map.put("college",college.getName());
+        map.put("direction",teacher.getDirection());
+        map.put("email",teacher.getEmail());
+        map.put("telephone",teacher.getPhoneNumber());
+        return  map;
+    }
+
+    public Map<String,Object> ToDoList(Long teacher_id){
+        Map<String,Object>map=new HashMap<String,Object>();
+        Teacher teacher=teacherMapper.selectByPrimaryKey(teacher_id);
+        map.put("emailexsit",(teacher.getEmail()!=null)?true:false);
+        map.put("headexsit",(teacher.getHeadPortrait()!=null)?true:false);
+        map.put("telephoneexsit",(teacher.getPhoneNumber()!=null)?true:false);
+
+        Boolean subjectUp=true,openReview=true,midReview=true,paperReview=true,crossReview=true,conReview=true;
+        SubjectExample subjectExample=new SubjectExample();
+        subjectExample.createCriteria().andCreateTeacherIdEqualTo(teacher.getId()).andStateEqualTo("RETURN");
+        List<Subject> subjects=subjectMapper.selectByExample(subjectExample);
+        subjectUp=(subjects.size()>0)?false:true;
+        
+        //review_teacher_id1
+        subjectExample.clear();
+        subjectExample.createCriteria().andReviewTeacherId1EqualTo(teacher_id);
+        List<Subject> subjects1=subjectMapper.selectByExample(subjectExample);
+        for (Subject subject : subjects1) {
+            StudentExample studentExample=new StudentExample();
+            studentExample.createCriteria().andSubjectIdEqualTo(subject.getId());
+            List<Student> students=studentMapper.selectByExample(studentExample);
+            for (Student student : students) {
+                String state=student.getState();
+                if(state.equals("WaitOpenScore")&&student.getOpenScore1()==null)openReview=false;
+                if(state.equals("WaitMidScore")&&student.getMiddleScore1()==null)midReview=false;
+                if(state.equals("WaitConScore")&&student.getConclusionScore1()==null)conReview=false;
+                if(openReview==false&&midReview==false&&conReview==false)break;
+            }
+            if(openReview==false&&midReview==false&&conReview==false)break;
+        }
+
+        //review_teacher_id2
+        subjectExample.clear();
+        subjectExample.createCriteria().andReviewTeacherId2EqualTo(teacher_id);
+        List<Subject> subjects2=subjectMapper.selectByExample(subjectExample);
+        for (Subject subject : subjects2) {
+            StudentExample studentExample=new StudentExample();
+            studentExample.createCriteria().andSubjectIdEqualTo(subject.getId());
+            List<Student> students=studentMapper.selectByExample(studentExample);
+            for (Student student : students) {
+                String state=student.getState();
+                if(state.equals("WaitOpenScore")&&student.getOpenScore2()==null)openReview=false;
+                if(state.equals("WaitMidScore")&&student.getMiddleScore2()==null)midReview=false;
+                if(state.equals("WaitConScore")&&student.getConclusionScore2()==null)conReview=false;
+                if(openReview==false&&midReview==false&&conReview==false)break;
+            }
+            if(openReview==false&&midReview==false&&conReview==false)break;
+        }
+
+        //review_teacher_id3
+        subjectExample.clear();
+        subjectExample.createCriteria().andReviewTeacherId3EqualTo(teacher_id);
+        List<Subject> subjects3=subjectMapper.selectByExample(subjectExample);
+        for (Subject subject : subjects3) {
+            StudentExample studentExample=new StudentExample();
+            studentExample.createCriteria().andSubjectIdEqualTo(subject.getId());
+            List<Student> students=studentMapper.selectByExample(studentExample);
+            for (Student student : students) {
+                String state=student.getState();
+                if(state.equals("WaitOpenScore")&&student.getOpenScore3()==null)openReview=false;
+                if(state.equals("WaitMidScore")&&student.getMiddleScore3()==null)midReview=false;
+                if(state.equals("WaitConScore")&&student.getConclusionScore3()==null)conReview=false;
+                if(openReview==false&&midReview==false&&conReview==false)break;
+            }
+            if(openReview==false&&midReview==false&&conReview==false)break;
+        }
+
+        //cross_review_teacher
+        subjectExample.clear();
+        subjectExample.createCriteria().andCrossReviewTeacherEqualTo(teacher_id);
+        List<Subject> subjects4=subjectMapper.selectByExample(subjectExample);
+        for (Subject subject : subjects4) {
+            StudentExample studentExample=new StudentExample();
+            studentExample.createCriteria().andSubjectIdEqualTo(subject.getId());
+            List<Student> students=studentMapper.selectByExample(studentExample);
+            for (Student student : students) {
+                String state=student.getState();
+                if(state.equals("WaitPaperScore")&&student.getCrossPaperScore()==null)crossReview=false;
+                if(crossReview==false)break;
+            }
+            if(crossReview==false)break;
+        }
+
+        //paper_review_teacher
+        subjectExample.clear();
+        subjectExample.createCriteria().andCreateTeacherIdEqualTo(teacher_id);
+        List<Subject> subjects5=subjectMapper.selectByExample(subjectExample);
+        for (Subject subject : subjects5) {
+            StudentExample studentExample=new StudentExample();
+            studentExample.createCriteria().andSubjectIdEqualTo(subject.getId());
+            List<Student> students=studentMapper.selectByExample(studentExample);
+            for (Student student : students) {
+                String state=student.getState();
+                if(state.equals("WaitPaperScore")&&student.getTeacherPaperScore()==null)paperReview=false;
+                if(paperReview==false)break;
+            }
+            if(paperReview==false)break;
+        }
+
+        map.put("subjectUp",subjectUp);
+        map.put("openReview",openReview);
+        map.put("midReview",midReview);
+        map.put("conReview",conReview);
+        map.put("crossReview",crossReview);
+        map.put("paperReview",paperReview);
+        return map;
+    }
+
+    public List<Map<String,Object>> getReview(Long teacher_id){
+        List<Teacher> teachers=teacherMapper.selectByExample(null);
+        Map<Long,String> teacher_map=new HashMap<Long,String>();
+        for (Teacher teacher : teachers) {
+            teacher_map.put(teacher.getId(),teacher.getName());
+        }
+        SubjectExample subjectExample=new SubjectExample();
+        subjectExample.or().andReviewTeacherId1EqualTo(teacher_id);
+        subjectExample.or().andReviewTeacherId2EqualTo(teacher_id);
+        subjectExample.or().andReviewTeacherId3EqualTo(teacher_id);
+        List<Subject> subjects=subjectMapper.selectByExample(subjectExample);
+        List<Map<String,Object>> list=new ArrayList<Map<String,Object>>();
+        for (Subject subject : subjects) {
+            Map<String,Object> map=new HashMap<String,Object>();
+            map.put("id",subject.getId());
+            map.put("name",subject.getName());
+            map.put("review1",teacher_map.get(subject.getReviewTeacherId1()));
+            map.put("review2",teacher_map.get(subject.getReviewTeacherId2()));
+            map.put("review3",teacher_map.get(subject.getReviewTeacherId3()));
+            list.add(map);
+        }
+        return list;
+    }
 }
