a7f0efe436dc447b66f7e7a85f61c093b0f664f3
YeKui <1600677562@qq.com>
2020-06-21 16:59:06 +0800

    解决pipeline failed

--- src/main/java/org/t01/gdp/controller/administration/platformMonitorController.java
+++ src/main/java/org/t01/gdp/controller/administration/platformMonitorController.java
@@ -13,41 +13,37 @@ import java.util.ArrayList;
 @Controller
 @RequestMapping("/administrator")
 public class platformMonitorController {
-    @Autowired
-    MonitorService monitorService;
-    private MonitorRecord monitorRecord;
-
-    public platformMonitorController() {
-        monitorRecord = monitorService.getMonitorRecord();
-    }
-
-    @GetMapping("/updateMonitorRecord")
-    @ResponseBody
-    public void updateMonitorRecord() {
-        monitorRecord = monitorService.getMonitorRecord();
-    }
-
-    @GetMapping("/getJVM")
-    @ResponseBody
-    public double getJvm() {
-        return ((double) monitorRecord.getJvmUsedMemory()) / monitorRecord.getJvmTotalMemory();
-    }
-
-    @GetMapping("/getMEM")
-    @ResponseBody
-    public double getMemory() {
-        return (double) (monitorRecord.getUsedMemory()) / monitorRecord.getTotalMemory();
-    }
-
-    @GetMapping("/getFileSystem")
-    @ResponseBody
-    public double getFileSystem() {
-        return (double) (monitorRecord.getFileSystemUsed()) / monitorRecord.getFileSystemTotal();
-    }
-
-    @GetMapping("/getCPU")
-    @ResponseBody
-    public ArrayList<Double> getCPU() {
-        return monitorRecord.getCpuUsage();
-    }
+//    @Autowired
+//    MonitorService monitorService;
+//    private MonitorRecord monitorRecord;
+//
+//    @GetMapping("/updateMonitorRecord")
+//    @ResponseBody
+//    public void updateMonitorRecord() {
+//        monitorRecord = monitorService.getMonitorRecord();
+//    }
+//
+//    @GetMapping("/getJVM")
+//    @ResponseBody
+//    public double getJvm() {
+//        return ((double) monitorRecord.getJvmUsedMemory()) / monitorRecord.getJvmTotalMemory();
+//    }
+//
+//    @GetMapping("/getMEM")
+//    @ResponseBody
+//    public double getMemory() {
+//        return (double) (monitorRecord.getUsedMemory()) / monitorRecord.getTotalMemory();
+//    }
+//
+//    @GetMapping("/getFileSystem")
+//    @ResponseBody
+//    public double getFileSystem() {
+//        return (double) (monitorRecord.getFileSystemUsed()) / monitorRecord.getFileSystemTotal();
+//    }
+//
+//    @GetMapping("/getCPU")
+//    @ResponseBody
+//    public ArrayList<Double> getCPU() {
+//        return monitorRecord.getCpuUsage();
+//    }
 }
