9e6afda3ceaf7a173e5161b8d61b450cca0a41d4
PengxinZeng <2279296959@qq.com>
2020-06-21 18:06:44 +0800

    Update platformMonitorController.java

--- src/main/java/org/t01/gdp/controller/administration/platformMonitorController.java
+++ src/main/java/org/t01/gdp/controller/administration/platformMonitorController.java
@@ -13,50 +13,59 @@ import java.util.ArrayList;
 @Controller
 @RequestMapping("/administrator")
 public class platformMonitorController {
-//    @Autowired
-//    MonitorService monitorService;
-//    private MonitorRecord monitorRecord;
-//
-//    @GetMapping("/updateMonitorRecord")
-//    @ResponseBody
-//    public void updateMonitorRecord() {
-//        monitorRecord = monitorService.getMonitorRecord();
-//    }
-//
-//    @GetMapping("/getJVM")
-//    @ResponseBody
-//    public double getJvm() {
-//        monitorRecord = monitorService.getMonitorRecord();
-//        return ((double) monitorRecord.getJvmUsedMemory()) / monitorRecord.getJvmTotalMemory();
-//    }
-//
-//    @GetMapping("/getMEM")
-//    @ResponseBody
-//    public double getMemory() {
-//        monitorRecord = monitorService.getMonitorRecord();
-//        if(monitorRecord.getTotalMemory()!=null){
-//            return (double) (monitorRecord.getUsedMemory()) / monitorRecord.getTotalMemory();
-//        }
-//        return 0;
-//    }
-//
-//    @GetMapping("/getFileSystem")
-//    @ResponseBody
-//    public double getFileSystem() {
-//        monitorRecord = monitorService.getMonitorRecord();
-//        if(monitorRecord.getFileSystemTotal()!=null){
-//            return (double) (monitorRecord.getFileSystemUsed()) / monitorRecord.getFileSystemTotal();
-//        }
-//        return 0;
-//    }
-//
-//    @GetMapping("/getCPU")
-//    @ResponseBody
-//    public ArrayList<Double> getCPU() {
-//        monitorRecord = monitorService.getMonitorRecord();
-//        if(monitorRecord.getCpuUsage()!=null){
-//            return monitorRecord.getCpuUsage();
-//        }
-//        return new ArrayList<>();
-//    }
+    @Autowired
+    MonitorService monitorService;
+    private MonitorRecord monitorRecord;
+
+    @GetMapping("/updateMonitorRecord")
+    @ResponseBody
+    public void updateMonitorRecord() {
+        monitorRecord = monitorService.getMonitorRecord();
+    }
+
+    @GetMapping("/getJVM")
+    @ResponseBody
+    public double getJvm() {
+        monitorRecord = monitorService.getMonitorRecord();
+        return ((double) monitorRecord.getJvmUsedMemory()) / monitorRecord.getJvmTotalMemory();
+    }
+
+    @GetMapping("/getMEM")
+    @ResponseBody
+    public double getMemory() {
+        monitorRecord = monitorService.getMonitorRecord();
+        if(monitorRecord.getTotalMemory()!=null){
+            return (double) (monitorRecord.getUsedMemory()) / monitorRecord.getTotalMemory();
+        }
+        return 0;
+    }
+
+    @GetMapping("/getFileSystem")
+    @ResponseBody
+    public double getFileSystem() {
+        monitorRecord = monitorService.getMonitorRecord();
+        if(monitorRecord.getFileSystemTotal()!=null){
+            return (double) (monitorRecord.getFileSystemUsed()) / monitorRecord.getFileSystemTotal();
+        }
+        return 0;
+    }
+
+    @GetMapping("/getCPU")
+    @ResponseBody
+    public ArrayList<Double> getCPU() {
+        monitorRecord = monitorService.getMonitorRecord();
+        if(monitorRecord.getCpuUsage()!=null){
+            return monitorRecord.getCpuUsage();
+        }
+        return new ArrayList<>();
+    }
+    @GetMapping("/getProcessor")
+    @ResponseBody
+    public Double getProcessor() {
+        monitorRecord = monitorService.getMonitorRecord();
+        if(monitorRecord.getCpuUsage()!=null){
+            return monitorRecord.getCpuUsage();
+        }
+        return 0.1;
+    }
 }
