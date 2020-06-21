1e2d1e31528e3da2e8d7a2a90c33d8b53388528a
YeKui <1600677562@qq.com>
2020-06-21 18:04:35 +0800

    测试

--- src/test/java/org/t01/gdp/GdpApplicationTests.java
+++ src/test/java/org/t01/gdp/GdpApplicationTests.java
@@ -1,20 +1,8 @@
 package org.t01.gdp;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.test.context.junit4.SpringRunner;
-import org.t01.gdp.config.MailProperties;
-import org.t01.gdp.service.MailService;
-import org.t01.gdp.service.VerificationService;
 
-import java.util.ArrayList;
-import java.util.Date;
-
-@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GdpApplicationTests {
 	@Test
