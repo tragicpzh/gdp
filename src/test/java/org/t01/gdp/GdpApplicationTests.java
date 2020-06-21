bb6bf5d306d5c226be78cf5a30e5f5be6579e1d8
YeKui <1600677562@qq.com>
2020-06-21 16:46:43 +0800

    添加libsigar-amd64-linux.so

--- src/test/java/org/t01/gdp/GdpApplicationTests.java
+++ src/test/java/org/t01/gdp/GdpApplicationTests.java
@@ -1,10 +1,12 @@
 package org.t01.gdp;
 
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.t01.gdp.config.MailProperties;
 import org.t01.gdp.service.MailService;
 import org.t01.gdp.service.VerificationService;
@@ -12,6 +14,7 @@ import org.t01.gdp.service.VerificationService;
 import java.util.ArrayList;
 import java.util.Date;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class GdpApplicationTests {
 	@Test
