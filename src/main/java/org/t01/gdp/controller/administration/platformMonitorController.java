92f777000b629799da6e87b3fd27de8c40356bb3
PengxinZeng <2279296959@qq.com>
2020-06-21 16:26:10 +0800

    Update platformMonitorController.java

--- src/main/java/org/t01/gdp/controller/administration/platformMonitorController.java
+++ src/main/java/org/t01/gdp/controller/administration/platformMonitorController.java
@@ -2,26 +2,53 @@ package org.t01.gdp.controller.administration;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.t01.gdp.domain.TimePoint;
+import org.t01.gdp.domain.MonitorRecord;
 import org.t01.gdp.service.MonitorService;
-import org.t01.gdp.service.TimeAxisService;
 
 import java.util.ArrayList;
 
 @Controller
 @RequestMapping("/administrator")
-
 public class platformMonitorController {
     @Autowired
     MonitorService monitorService;
+    private MonitorRecord monitorRecord;
 
-    @GetMapping("/getLog")
+    public platformMonitorController() {
+        monitorService = new MonitorService();
+        monitorRecord = monitorService.getMonitorRecord();
+    }
+
+    @GetMapping("/updateMonitorRecord")
     @ResponseBody
-    public String getLog() {
-        return monitorService.getLog();
+    public void updateMonitorRecord() {
+        monitorRecord = monitorService.getMonitorRecord();
+    }
+
+    @GetMapping("/getJVM")
+    @ResponseBody
+    public double getJvm() {
+        return ((double) monitorRecord.getJvmUsedMemory()) / monitorRecord.getJvmTotalMemory();
+    }
+
+    @GetMapping("/getMEM")
+    @ResponseBody
+    public double getMemory() {
+        return (double) (monitorRecord.getUsedMemory()) / monitorRecord.getTotalMemory();
+    }
+
+    @GetMapping("/getFileSystem")
+    @ResponseBody
+    public double getFileSystem() {
+        return (double) (monitorRecord.getFileSystemUsed()) / monitorRecord.getFileSystemTotal();
+    }
+
+    @GetMapping("/getCPU")
+    @ResponseBody
+    public ArrayList<Double> getCPU() {
+        return monitorRecord.getCpuUsage();
     }
 }
